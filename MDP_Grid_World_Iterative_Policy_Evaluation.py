14c33cc2cdeafebce7d1e43f82f9560ba59e9de2
and-bar <58216241+and-bar@users.noreply.github.com>
2021-06-02 22:45:29 +0200

    New function on searching best policy (#1)

--- MDP_Grid_World_Iterative_Policy_Evaluation.py
+++ MDP_Grid_World_Iterative_Policy_Evaluation.py
@@ -11,8 +11,8 @@ np.set_printoptions(linewidth=np.inf)
 # zeros agent can pass and ones it a wall
 # Generate Grid world till terminal states : last horisontal to the right and first two vertical of the top not the walls
 
-vertical_dim = 10
-horizontal_dim = 10
+vertical_dim = 300
+horizontal_dim = 100
 digit_for_representing_the_wall = 1
 
 # Announsing terminal states
@@ -20,14 +20,16 @@ terminal_states = [(0, horizontal_dim - 1), (1, horizontal_dim - 1)]
 
 grid_world_ndarray = np.zeros([vertical_dim, horizontal_dim])
 counter_walls = 0
-while counter_walls <= 10:
+while counter_walls <= int(vertical_dim*horizontal_dim*0.1):
     grid_world_ndarray[random.randint(0,vertical_dim-1) , random.randint(0,horizontal_dim-1)] = digit_for_representing_the_wall
     counter_walls += 1
 
+# cleaning walls in start space and terminal spaces
 grid_world_ndarray[0,horizontal_dim - 1] = 0
 grid_world_ndarray[1,horizontal_dim - 1] = 0
 grid_world_ndarray[0,horizontal_dim - 2] = 0
 grid_world_ndarray[1,horizontal_dim - 2] = 0
+grid_world_ndarray[vertical_dim-1,0] = 0
 
 plt.imshow(grid_world_ndarray, interpolation='none')
 plt.savefig('gridworld.png')
@@ -37,7 +39,7 @@ V_of_S_for_every_state_ndarray = np.zeros([vertical_dim, horizontal_dim])
 
 # Creating grid of rewards of every state
 rewards_ndarray = np.zeros([vertical_dim, horizontal_dim])
-rewards_ndarray[0,horizontal_dim - 1] = 1
+rewards_ndarray[0,horizontal_dim - 1] = 100
 rewards_ndarray[1,horizontal_dim - 1] = -1
 
 def find_all_actions_of_the_state(tuple_state_for_searching):
@@ -108,9 +110,37 @@ def iterative_policy_evaluation():
 iterative_policy_evaluation()
 
 # dumping V of S of all states to excel
-V_of_S_for_every_state_df = pd.DataFrame(V_of_S_for_every_state_ndarray)
-filepath = 'V_of_S_for_every_state_ndarray.xlsx'
-V_of_S_for_every_state_df.to_excel(filepath, index=False)
+# V_of_S_for_every_state_df = pd.DataFrame(V_of_S_for_every_state_ndarray)
+# filepath = 'V_of_S_for_every_state_ndarray.xlsx'
+# V_of_S_for_every_state_df.to_excel(filepath, index=False)
 
-plt.imshow(V_of_S_for_every_state_df, interpolation='none')
-plt.savefig('V_of_S_space_states.png')
+# plt.imshow(V_of_S_for_every_state_df, interpolation='none')
+# plt.savefig('V_of_S_space_states.png')
+
+def search_best_policy_from_input_state():
+    """
+    Function get as input the coordinates of statrting state and
+    searching the best policy and saves it and vusualizes it
+    """
+    state_vert_coord = vertical_dim - 1
+    state_horizont_coord  = 0
+    
+    grid_best_policy_ndrray[state_vert_coord, state_horizont_coord] = 1
+    v_of_s_tuple = (state_vert_coord, state_horizont_coord)
+    while v_of_s_tuple != (0, horizontal_dim-2):
+        actions_of_state_tuple = find_all_actions_of_the_state(v_of_s_tuple)
+        # print(f"possible actions {actions_of_state_tuple} for state {v_of_s_tuple}")
+        v_of_s_tuple =  actions_of_state_tuple[0]
+        for action in actions_of_state_tuple[1:]:
+            if V_of_S_for_every_state_ndarray[action] > V_of_S_for_every_state_ndarray[v_of_s_tuple]:
+                v_of_s_tuple = action
+        grid_best_policy_ndrray[v_of_s_tuple] = 1
+        # print(f"next state: {v_of_s_tuple}")
+        # input("input for the next loop")
+    
+    plt.imshow(grid_best_policy_ndrray, interpolation='none')
+    plt.savefig('best_policy.png')
+
+
+grid_best_policy_ndrray = np.zeros((vertical_dim,horizontal_dim))
+search_best_policy_from_input_state()
